0d4f364dece4310fa4bfd551f1abb11d2b2a412b
Vaishnavi Nagrale <Vaishnavinagrale2@gmail.com>
2024-06-13 17:17:24 +0530

    first commit to done with all changes and deploying the project

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000/api/v1",
+        target: "https://nova-bnpz.onrender.com/api/v1",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
